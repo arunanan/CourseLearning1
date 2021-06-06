e8527c17b7d6c000594f3faf57c5c7770e8f4247
arunanan <sarun462@gmail.com>
2021-06-06 22:40:23 +0530

    Create Encapsulation.java

---
+++ src/com/company/ClassesAndObjects/Encapsulation.java
@@ -0,0 +1,9 @@
+
+
+//Access Modifiers:
+
+//Public: When you declare a variable or a method as public in a class, it signifies that it can be accessed throughout the class. You can access these variables and methods from other classes as well
+//Private: When you declare a variable as private in a class, it signifies that it can be accessed throughout the class but not outside of it.
+
+/*Getter and Setters:
+    used for accessing private variables of the class. So does it make private variable to Public? No beacuse we can have sanity checking or contraints that can be put into setter methods so that variables are assigned only when the pass sanity check.*/
