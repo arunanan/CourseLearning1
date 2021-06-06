5cc78bf3dccf97eb775ca07af1a76278bddf0b96
arunanan <sarun462@gmail.com>
2021-06-06 23:21:21 +0530

    Update Encapsulation.java

--- src/com/company/ClassesAndObjects/Encapsulation.java
+++ src/com/company/ClassesAndObjects/Encapsulation.java
@@ -9,6 +9,9 @@
     used for accessing private variables of the class. Does it make private variable to Public? No beacuse we can have sanity checking or contraints that 
     can be put into setter methods so that variables are assigned only when the pass sanity check.*/
 
+/*Encapsulation is a principle based on hiding the state of objects and restricting their access to various parts of your program. 
+    Encapsulation is achieved with the use of private access modifiers and the getter and setter methods.*/
+
 // Abstraction vs Encapsulation
 /*Abstraction concept makes code more user friendly by hiding implementation part of the class, which makes program more robust where user can use class without thinking of its implementation.    
 
